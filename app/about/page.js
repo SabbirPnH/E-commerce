6380fa17b40844408f888ddc4abd779d4e306c5c
SabbirPnH <mdsabbirrahmanm740@gmail.com>
2024-07-14 11:35:16 -0700

    about page name change

--- app/about/page.js
+++ app/about/page.js
@@ -15,7 +15,7 @@ const AboutUs = () => {
          <div className="relative px-4  sm:px-20 py-10 sm:py-16 lg:py-28  bg-custom  bg-cover bg-center bg-no-repeat">
             <div className="absolute inset-0 bg-black bg-opacity-80"></div>
             <div className="relative z-10">
-                <h2 className="text-[26px] sm:text-32px] text-[#E9FF97]">About Us</h2>
+                <h2 className="text-[26px] sm:text-32px] text-[#E9FF97]">PnH About Us</h2>
                 <p className="text-white py-2 lg:w-1/2">
                     PnH Corporation is a reputable software company with years of rich experience developing web design development and software for their customers on demand.
                 </p>
